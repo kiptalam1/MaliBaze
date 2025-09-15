4e40c444b2c1dafb603e7e8b5d41dd5efb420235
kiptalam1 <adamskiptalam0@gmail.com>
2025-09-15 15:08:46 +0300

    feat(user): allow user to update email and name. - added a function that sanitizes user input and ignores empty fields. - added a controller that updates the user profile. - user can only update own profile. - created validation for the optional input fields. - if the provided fields are not different then the db is not called and the document remains unchanged.

--- server/src/controllers/user.controllers.ts
+++ server/src/controllers/user.controllers.ts
@@ -1,6 +1,7 @@
 import type { AuthenticatedRequest } from "../middlewares/auth.middlewares.js";
 import User, { type IUser } from "../models/user.model.js";
 import type { Response } from "express";
+import type { JWTPayload } from "../utils/token.utils.js";
 
 export const getMyProfile = async (
 	req: AuthenticatedRequest,
@@ -24,5 +25,83 @@ export const getMyProfile = async (
 		return res.status(500).json({
 			error: "Internal server error",
 		});
+	}
+};
+
+// utility to remove undefined keys from req.body;
+function removeUndefined<T extends Record<string, any>>(obj: T): Partial<T> {
+	return Object.fromEntries(
+		Object.entries(obj).filter(([_, v]) => v !== undefined)
+	) as Partial<T>;
+}
+
+export const updateMyProfile = async (
+	req: AuthenticatedRequest,
+	res: Response
+): Promise<Response | void> => {
+	const { userId } = req.user as JWTPayload;
+
+	try {
+		// check if user exists;
+		const user = await User.findById(userId).select(
+			"-password -__v -refreshToken"
+		);
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		// sanitize update fields;
+		const updates = removeUndefined({
+			name: req.body.name,
+			email: req.body.email,
+		});
+
+		//filter out unchanged fields;
+		const effectiveUpdates: Partial<typeof updates> = {};
+		for (const key of Object.keys(updates) as (keyof typeof updates)[]) {
+			if (updates[key] !== user[key]) {
+				effectiveUpdates[key] = updates[key];
+			}
+		}
+
+		// if email is being updated check uniqueness;
+		if (effectiveUpdates.email) {
+			const emailExists = await User.findOne({
+				email: effectiveUpdates.email,
+				_id: { $ne: userId },
+			}).select("_id");
+
+			if (emailExists) {
+				return res
+					.status(400)
+					.json({ error: "This email already exists. Try another email" });
+			}
+		}
+
+		// if no more updates left, skip DB call;
+		if (Object.keys(effectiveUpdates).length === 0) {
+			return res.status(400).json({
+				error: "No changes made",
+			});
+		}
+
+		// else proceed;
+		const updatedUser = await User.findByIdAndUpdate(
+			userId,
+			{
+				$set: effectiveUpdates,
+			},
+			{
+				returnDocument: "after",
+			}
+		).select("-password -refreshToken -__v");
+
+		return res.status(200).json({
+			message: "Profile updated successfully",
+			user: updatedUser,
+		});
+	} catch (error) {
+		console.error("Error updating user profile", (error as Error).message);
+		return res.status(500).json({ error: "Internal server error" });
 	}
 };
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,9 +1,19 @@
 import express from "express";
-import { getMyProfile } from "../controllers/user.controllers.js";
+import {
+	getMyProfile,
+	updateMyProfile,
+} from "../controllers/user.controllers.js";
 import { authenticateUser } from "../middlewares/auth.middlewares.js";
+import { userProfileUpdateInputValidators } from "../validators/user.validators.js";
 
 const router = express.Router();
 
 router.get("/me", authenticateUser, getMyProfile);
+router.patch(
+	"/update-profile",
+	authenticateUser,
+	userProfileUpdateInputValidators,
+	updateMyProfile
+);
 
 export default router;
