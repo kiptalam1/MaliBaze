446e5bc5bb761e6410b4fbb7b1bd6d3d130e0a2e
kiptalam1 <adamskiptalam0@gmail.com>
2025-09-15 18:25:18 +0300

    feat(product): add sku field to product schema

--- server/src/models/product.model.ts
+++ server/src/models/product.model.ts
@@ -1,5 +1,15 @@
 import mongoose, { Document, Types, Model } from "mongoose";
 
+export interface IProduct extends Document {
+	_id: Types.ObjectId;
+	name: string;
+	description: string;
+	price: number;
+	category: Types.ObjectId;
+	rating?: number | null;
+	sku: string;
+}
+
 const ProductSchema = new mongoose.Schema<IProduct>(
 	{
 		name: {
@@ -27,21 +37,20 @@ const ProductSchema = new mongoose.Schema<IProduct>(
 			min: 0,
 			max: 5,
 		},
+		sku: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+		},
 	},
 	{ timestamps: true }
 );
 
-export interface IProduct extends Document {
-	_id: Types.ObjectId;
-	name: string;
-	description: string;
-	price: number;
-	category: Types.ObjectId;
-	rating?: number | null;
-}
-
 ProductSchema.index({ category: 1 });
 ProductSchema.index({ name: "text", description: "text" });
+ProductSchema.index({ sku: 1 }, { unique: true });
+
 
 const Product: Model<IProduct> =
 	mongoose.models["Product"] ||
